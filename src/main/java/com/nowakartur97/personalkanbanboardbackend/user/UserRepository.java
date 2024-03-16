543d2634b830e9e65b458a7ba7dc74099a90b13e
arnk <artur.nowak@gft.com>
2024-03-16 11:34:28 +0100

    Create methods in repositories and response model

--- src/main/java/com/nowakartur97/personalkanbanboardbackend/user/UserRepository.java
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/user/UserRepository.java
@@ -2,9 +2,11 @@ package com.nowakartur97.personalkanbanboardbackend.user;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends R2dbcRepository<UserEntity, UUID> {
+    Mono<UserEntity> findByUsername(String username);
 }
