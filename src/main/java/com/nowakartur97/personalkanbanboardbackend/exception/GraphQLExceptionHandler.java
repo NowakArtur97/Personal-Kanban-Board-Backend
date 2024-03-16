796606b316922e8b7aaa0d6e1c0d061df0b3bd68
arnk <artur.nowak@gft.com>
2024-03-16 12:15:30 +0100

    Create custom exception handler

---
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/exception/GraphQLExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.nowakartur97.personalkanbanboardbackend.exception;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof ResourceNotFoundException) {
+            return mapToGraphQLError(ErrorType.NOT_FOUND, ex, env);
+        }
+        return null;
+    }
+
+    private GraphQLError mapToGraphQLError(ErrorType errorType, Throwable ex, DataFetchingEnvironment env) {
+        return GraphqlErrorBuilder.newError()
+                .errorType(errorType)
+                .message(ex.getMessage())
+                .path(env.getExecutionStepInfo().getPath())
+                .location(env.getField().getSourceLocation())
+                .build();
+    }
+}
