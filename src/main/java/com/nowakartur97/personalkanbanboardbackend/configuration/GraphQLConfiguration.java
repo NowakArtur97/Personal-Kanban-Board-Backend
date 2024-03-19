06ca930db8e9fc314e7c0fbd31a8b65ec1843085
arnk <artur.nowak@gft.com>
2024-03-19 12:44:19 +0100

    Add validation to user dto fields so they can't be blank

--- src/main/java/com/nowakartur97/personalkanbanboardbackend/configuration/GraphQLConfiguration.java
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/configuration/GraphQLConfiguration.java
@@ -1,6 +1,9 @@
 package com.nowakartur97.personalkanbanboardbackend.configuration;
 
 import graphql.scalars.ExtendedScalars;
+import graphql.validation.rules.OnValidationErrorStrategy;
+import graphql.validation.rules.ValidationRules;
+import graphql.validation.schemawiring.ValidationSchemaWiring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -10,7 +13,13 @@ public class GraphQLConfiguration {
 
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        ValidationRules validationRules = ValidationRules.newValidationRules()
+                .onValidationErrorStrategy(OnValidationErrorStrategy.RETURN_NULL)
+                .build();
+        ValidationSchemaWiring validationSchemaWiring = new ValidationSchemaWiring(validationRules);
+
         return wiringBuilder -> wiringBuilder
-                .scalar(ExtendedScalars.Date);
+                .scalar(ExtendedScalars.Date)
+                .directiveWiring(validationSchemaWiring);
     }
 }
