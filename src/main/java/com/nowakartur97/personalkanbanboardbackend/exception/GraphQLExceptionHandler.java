eb2001e90ff79a63e68e1a193c06bc1e8d4aebe8
arnk <artur.nowak@gft.com>
2024-03-19 18:00:38 +0100

    Handle ConstraintViolationException to multiple GraphQLErrors

--- src/main/java/com/nowakartur97/personalkanbanboardbackend/exception/GraphQLExceptionHandler.java
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/exception/GraphQLExceptionHandler.java
@@ -8,23 +8,46 @@ import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
 import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    private final static String CONSTRAINT_VALIDATION_EXCEPTION_MESSAGE_SEPARATOR = ",";
+    private final static String CONSTRAINT_VALIDATION_EXCEPTION_MESSAGE_PREFIX = ": ";
 
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
         if (ex instanceof ResourceNotFoundException) {
-            return mapToGraphQLError(ErrorType.NOT_FOUND, ex, env);
-        } else if (ex instanceof ConstraintViolationException) {
-            return mapToGraphQLError(ErrorType.BAD_REQUEST, ex, env);
+            return createGraphQLError(ErrorType.NOT_FOUND, ex.getMessage(), env);
         }
         return null;
     }
 
-    private GraphQLError mapToGraphQLError(ErrorType errorType, Throwable ex, DataFetchingEnvironment env) {
+    @Override
+    protected List<GraphQLError> resolveToMultipleErrors(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof ConstraintViolationException) {
+            return mapToGraphQLErrors(ErrorType.BAD_REQUEST, ex, env);
+        }
+        return null;
+    }
+
+    private List<GraphQLError> mapToGraphQLErrors(ErrorType errorType,
+                                                  Throwable ex,
+                                                  DataFetchingEnvironment env) {
+        return Arrays.stream(ex.getMessage().split(CONSTRAINT_VALIDATION_EXCEPTION_MESSAGE_SEPARATOR))
+                .map(String::trim)
+                .map(msg -> msg.substring(msg.indexOf(CONSTRAINT_VALIDATION_EXCEPTION_MESSAGE_PREFIX)
+                        + CONSTRAINT_VALIDATION_EXCEPTION_MESSAGE_PREFIX.length()))
+                .map(msg -> createGraphQLError(errorType, msg, env))
+                .toList();
+    }
+
+    private GraphQLError createGraphQLError(ErrorType errorType, String message, DataFetchingEnvironment env) {
         return GraphqlErrorBuilder.newError()
                 .errorType(errorType)
-                .message(ex.getMessage())
+                .message(message)
                 .path(env.getExecutionStepInfo().getPath())
                 .location(env.getField().getSourceLocation())
                 .build();
