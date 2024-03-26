4571ff5190e2dd3aa1a98d4f4c9488f9f104e26b
arnk <artur.nowak@gft.com>
2024-03-26 17:07:08 +0100

    Create abstract user controller

--- src/main/java/com/nowakartur97/personalkanbanboardbackend/user/AuthenticationController.java
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/user/AuthenticationController.java
@@ -2,7 +2,6 @@ package com.nowakartur97.personalkanbanboardbackend.user;
 
 import com.nowakartur97.personalkanbanboardbackend.auth.JWTConfigurationProperties;
 import com.nowakartur97.personalkanbanboardbackend.auth.JWTUtil;
-import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -11,13 +10,12 @@ import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
 @Controller
-@RequiredArgsConstructor
-public class AuthenticationController {
+public class AuthenticationController extends UserController {
 
-    private final UserService userService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JWTUtil jwtUtil;
-    private final JWTConfigurationProperties jwtConfigurationProperties;
+    public AuthenticationController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder,
+                                    JWTUtil jwtUtil, JWTConfigurationProperties jwtConfigurationProperties) {
+        super(userService, bCryptPasswordEncoder, jwtUtil, jwtConfigurationProperties);
+    }
 
     @QueryMapping
     public Mono<UserResponse> loginUser(@Argument AuthenticationRequest authenticationRequest) {
@@ -32,13 +30,5 @@ public class AuthenticationController {
 
     private boolean isPasswordIncorrect(AuthenticationRequest authenticationRequest, UserEntity userEntity) {
         return !bCryptPasswordEncoder.matches(authenticationRequest.getPassword(), userEntity.getPassword());
-    }
-
-    private UserResponse mapToUserResponse(UserEntity userEntity) {
-        return new UserResponse(
-                userEntity.getUsername(),
-                userEntity.getEmail(),
-                jwtUtil.generateToken(userEntity.getUsername(), userEntity.getRole().name()),
-                jwtConfigurationProperties.getExpirationTimeInMilliseconds());
     }
 }
