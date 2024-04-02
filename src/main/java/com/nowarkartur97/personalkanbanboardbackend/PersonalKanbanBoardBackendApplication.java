972544cbfa64ff074531e100a63bd769a377eeef
arnk <nowart97@gmail.com>
2024-04-02 17:53:02 +0200

    Generate project using Spring Initializr

---
+++ src/main/java/com/nowarkartur97/personalkanbanboardbackend/PersonalKanbanBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nowarkartur97.personalkanbanboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalKanbanBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalKanbanBoardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowarkartur97/personalkanbanboardbackend/PersonalKanbanBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowarkartur97.personalkanbanboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalKanbanBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
