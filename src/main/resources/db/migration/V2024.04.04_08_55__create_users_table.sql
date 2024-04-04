f068dbe4b7ca02bbac2e137015926d6f2215bb02
arnk <nowart97@gmail.com>
2024-04-04 14:32:56 +0200

    Fix issue with primary keys being of type UUID

--- src/main/resources/db/migration/V2024.04.04_08_55__create_users_table.sql
+++ src/main/resources/db/migration/V2024.04.04_08_55__create_users_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE personal_kanban_board.users (
-    "user_id" SERIAL PRIMARY KEY,
+    "user_id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "username" VARCHAR(100) NOT NULL UNIQUE,
     "password" VARCHAR(100) NOT NULL,
     "email" VARCHAR(100) NOT NULL UNIQUE,
--- src/main/resources/db/migration/V2024.04.04_09_05__create_tasks_table.sql
+++ src/main/resources/db/migration/V2024.04.04_09_05__create_tasks_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE personal_kanban_board.tasks (
-    "task_id" SERIAL PRIMARY KEY,
+"task_id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "title" VARCHAR(100) NOT NULL,
     "description" VARCHAR(100),
     "status" VARCHAR(50) NOT NULL,
     "priority" VARCHAR(20) NOT NULL,
     "target_end_date" DATE NOT NULL,
-    "assigned_to" SERIAL NOT NULL,
-    "created_by" SERIAL NOT NULL,
+    "assigned_to" UUID NOT NULL,
+    "created_by" UUID NOT NULL,
     "created_on" DATE NOT NULL,
-    "modified_by" SERIAL,
+    "modified_by" UUID,
     "modified_on" DATE,
 
     CONSTRAINT "fk_tasks_assigned_to" FOREIGN KEY ("assigned_to") REFERENCES personal_kanban_board.users("user_id") ON DELETE CASCADE ON UPDATE NO ACTION,
