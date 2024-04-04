5c2f5e61f3be5dc7a76dabcd9270acce4c76b37b
arnk <nowart97@gmail.com>
2024-04-04 12:14:44 +0200

    Create user model

---
+++ src/main/java/com/nowarkartur97/personalkanbanboardbackend/user/UserEntity.java
@@ -0,0 +1,30 @@
+package com.nowarkartur97.personalkanbanboardbackend.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", schema = "personal_kanban_board")
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @Column(name = "user_id", updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(nullable = false, length = 100)
+    private String password;
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+}
