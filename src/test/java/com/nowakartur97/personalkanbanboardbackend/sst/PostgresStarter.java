4753f0d02bd11c3fb3ba4f140cacfaa76929d85b
arnk <nowart97@gmail.com>
2024-04-22 14:35:43 +0200

    Clean up postgres starter

--- src/test/java/com/nowakartur97/personalkanbanboardbackend/sst/PostgresStarter.java
+++ src/test/java/com/nowakartur97/personalkanbanboardbackend/sst/PostgresStarter.java
@@ -8,19 +8,16 @@ import org.testcontainers.utility.DockerImageName;
 public interface PostgresStarter {
 
     String POSTGRES_DOCKER_IMAGE = "postgres:14.1-alpine";
-    PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer(DockerImageName.parse(POSTGRES_DOCKER_IMAGE))
-            .withUsername("postgres")
-            .withPassword("postgres")
-            .withDatabaseName("kanban_board");
+    PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer(DockerImageName.parse(POSTGRES_DOCKER_IMAGE));
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.r2dbc.url", () ->
-                postgresContainer.getJdbcUrl().replace("jdbc", "r2dbc"));
         registry.add("spring.r2dbc.username", postgresContainer::getUsername);
         registry.add("spring.r2dbc.password", postgresContainer::getPassword);
-        registry.add("spring.flyway.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.flyway.username", postgresContainer::getUsername);
+        registry.add("spring.r2dbc.url", () ->
+                postgresContainer.getJdbcUrl().replace("jdbc", "r2dbc"));
+        registry.add("spring.flyway.user", postgresContainer::getUsername);
         registry.add("spring.flyway.password", postgresContainer::getPassword);
+        registry.add("spring.flyway.url", postgresContainer::getJdbcUrl);
     }
 }
