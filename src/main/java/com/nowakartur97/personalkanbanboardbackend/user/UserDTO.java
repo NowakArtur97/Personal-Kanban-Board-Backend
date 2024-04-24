7dfda3b2d55553df08df71a6ed32c7fc3880090b
arnk <nowart97@gmail.com>
2024-04-24 18:37:48 +0200

    Add validation to email format

--- src/main/java/com/nowakartur97/personalkanbanboardbackend/user/UserDTO.java
+++ src/main/java/com/nowakartur97/personalkanbanboardbackend/user/UserDTO.java
@@ -1,5 +1,6 @@
 package com.nowakartur97.personalkanbanboardbackend.user;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -11,5 +12,6 @@ public class UserDTO {
 
     private String username;
     private String password;
+    @Email(message = "{registerUser.userDTO.email.correctFormat}")
     private String email;
 }
