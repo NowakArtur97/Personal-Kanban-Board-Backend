0befab4640c1153882a2e3ed1a4da3cacbd926ac
arnk <nowart97@gmail.com>
2024-05-08 22:46:09 +0200

    Change start server command

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8080 -Dspring.profiles.active=local \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo java -jar -Dserver.port=8080 -Dspring.profiles.active=local \
+#    *.jar > /dev/null 2> /dev/null < /dev/null &
+
+nohup java -jar -Dserver.port=8080 -Dspring.profiles.active=local /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
