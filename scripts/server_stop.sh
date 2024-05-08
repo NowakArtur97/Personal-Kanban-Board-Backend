0d5cdb3503b0e2eb1f54da55ceb87cf2cf98c0e7
arnk <nowart97@gmail.com>
2024-05-08 23:10:27 +0200

    Change ApplicationStop hook script

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo pkill -f 'java -jar'
+#sudo pkill -f 'java -jar'
+isExistApp=$(pgrep java)
+if [[ -n  $isExistApp ]]; then
+  echo "Stopping the application."
+  sudo kill -9 $isExistApp
+  echo "The application is stopped."
+else
+  echo "The application is not running."
+fi
