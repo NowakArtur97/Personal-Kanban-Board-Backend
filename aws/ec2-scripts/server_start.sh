45dfeb49d10b661f2310b0f7e16625ad514a4b2e
NowakArtur97 <nowart97@gmail.com>
2024-05-30 11:24:07 +0200

    Remove local profile from startup command and add env variables to properties file

--- aws/ec2-scripts/server_start.sh
+++ aws/ec2-scripts/server_start.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/server
 #sudo java -jar -Dserver.port=8080 -Dspring.profiles.active=local \
 #    *.jar > /dev/null 2> /dev/null < /dev/null &
 
-nohup java -jar -Dserver.port=8080 -Dspring.profiles.active=local /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar -Dserver.port=8080 /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
