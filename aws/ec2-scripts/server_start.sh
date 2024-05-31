cca9c7dc9ee5fab987ae497bcac6ac77665f1030
NowakArtur97 <nowart97@gmail.com>
2024-05-31 20:10:20 +0200

    Clean up server start script

--- aws/ec2-scripts/server_start.sh
+++ aws/ec2-scripts/server_start.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 
-nohup java -jar -Dserver.port=8080 /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-
-sleep 10
-
-isExistApp=$(pgrep java)
-if [[ -n  $isExistApp ]]; then
-  echo "Stopping the application."
-  sudo kill -9 $isExistApp
-  echo "The application is stopped."
-else
-  echo "The application is not running."
-fi
-
-nohup java -jar -Dserver.port=8080 /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+java -jar -Dserver.port=8080 /home/ec2-user/server/personalkanbanboardbackend-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
