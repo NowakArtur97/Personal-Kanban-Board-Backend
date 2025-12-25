420cfa694b6a8da17b2e9d79ac58511f83aceac1
NowakArtur97 <nowart97@gmail.com>
2025-12-25 12:42:32 +0100

    Assert emitted event when deleting all tasks

--- src/test/java/com/nowakartur97/personalkanbanboardbackend/task/AllTasksDeletionMutationControllerTest.java
+++ src/test/java/com/nowakartur97/personalkanbanboardbackend/task/AllTasksDeletionMutationControllerTest.java
@@ -1,17 +1,29 @@
 package com.nowakartur97.personalkanbanboardbackend.task;
 
-import com.nowakartur97.personalkanbanboardbackend.common.test.TaskIntegrationTest;
+import com.nowakartur97.personalkanbanboardbackend.common.BaseTaskEvent;
+import com.nowakartur97.personalkanbanboardbackend.common.TaskEventType;
+import com.nowakartur97.personalkanbanboardbackend.common.test.TaskSubscriptionIntegrationTest;
 import com.nowakartur97.personalkanbanboardbackend.user.UserEntity;
 import com.nowakartur97.personalkanbanboardbackend.user.UserRole;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.time.Duration;
+import java.util.UUID;
 
 import static com.nowakartur97.personalkanbanboardbackend.common.test.GraphQLQueries.DELETE_ALL_TASKS;
+import static com.nowakartur97.personalkanbanboardbackend.common.test.GraphQLQueries.TASK_EVENT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class AllTasksDeletionMutationControllerTest extends TaskIntegrationTest<TaskEntity> {
+public class AllTasksDeletionMutationControllerTest extends TaskSubscriptionIntegrationTest<TaskEntity> {
 
     public AllTasksDeletionMutationControllerTest() {
-        super("deleteAllTasks", DELETE_ALL_TASKS, null);
+        super("deleteAllTasks", DELETE_ALL_TASKS, null,
+                TASK_EVENT, "taskEvent", TaskEvent.class);
     }
 
     @Test
@@ -22,9 +34,7 @@ public class AllTasksDeletionMutationControllerTest extends TaskIntegrationTest<
         createTask(admin.getUserId());
         createTask(userEntity.getUserId());
 
-        sendDeleteAllTasksRequest(admin);
-
-        assertThat(taskRepository.count().block()).isZero();
+        assertAllTasksDeletionAndSubscription(admin);
     }
 
     @Test
@@ -32,9 +42,7 @@ public class AllTasksDeletionMutationControllerTest extends TaskIntegrationTest<
 
         UserEntity admin = createUser("testAdmin", "testAdmin@domain.com", UserRole.ADMIN);
 
-        sendDeleteAllTasksRequest(admin);
-
-        assertThat(taskRepository.count().block()).isZero();
+        assertAllTasksDeletionAndSubscription(admin);
     }
 
     @Test
@@ -47,5 +55,32 @@ public class AllTasksDeletionMutationControllerTest extends TaskIntegrationTest<
 
     private void sendDeleteAllTasksRequest(UserEntity userEntity) {
         sendRequest(userEntity, document, path, null, null, false);
+    }
+
+    private void assertAllTasksDeletionAndSubscription(UserEntity userEntity) {
+        Flux<BaseTaskEvent> eventFlux = createWebSocketGraphQlTester(userEntity)
+                .document(subscriptionDocument)
+                .executeSubscription().toFlux()
+                .map(r -> (BaseTaskEvent) r.path(subscriptionPath).entity(subscriptionEntityType).get());
+
+        Mono<UUID> mutationMono = Mono.fromCallable(() -> {
+            sendDeleteAllTasksRequest(userEntity);
+            return UUID.randomUUID();
+        });
+
+        Flux<Tuple2<Long, BaseTaskEvent>> combined = eventFlux
+                .take(1)
+                .zipWith(mutationMono)
+                .flatMap(tuple ->
+                        repository.count()
+                                .map(count -> Tuples.of(count, tuple.getT1()))
+                );
+        StepVerifier.create(combined)
+                .assertNext(tuple -> {
+                    assertThat(tuple.getT1()).isZero();
+                    assertThat(tuple.getT2().getTaskEventType()).isEqualTo(TaskEventType.DELETE_ALL);
+                })
+                .thenCancel()
+                .verify(Duration.ofSeconds(5));
     }
 }
